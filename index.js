eef7d19f88c1c75c395c1f03213845579bccb3db
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-10-17 23:43:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ var theFunk = funkyFunction() {
 
 /*
 var animal = 'dog';
- 
+
 function makeZoo() {
   var animal = 'cat';
- 
+
   console.log(`I think I'll put this ${animal} in the zoo.`);
 }
- 
+
 makeZoo(); // "I think I'll put this cat in the zoo."
- 
+
 animal // "dog"
 */
