8b6f1432f24dffaa6b44488d6c92ce3e09e79586
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-10-17 23:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,23 +28,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction() {
-  var theFunk = 'FUNKY!'
-
-  console.log(theFunk);
-}
-
-
-/*
-var animal = 'dog';
-
-function makeZoo() {
-  var animal = 'cat';
-
-  console.log(`I think I'll put this ${animal} in the zoo.`);
-}
-
-makeZoo(); // "I think I'll put this cat in the zoo."
-
-animal // "dog"
-*/
+var theFunk = funkyFunction()();
+  
